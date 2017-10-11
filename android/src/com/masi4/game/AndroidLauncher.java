101fe1e49c9c4296d59e6cb742ba6e091eae00bd
masi4 <meowsqwe@gmail.com>
2017-10-11 21:18:19 +0400

    Добавлена папка NOTES с заметками

--- android/src/com/masi4/game/AndroidLauncher.java
+++ android/src/com/masi4/game/AndroidLauncher.java
@@ -6,11 +6,14 @@ import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.masi4.game.GameMainClass;
 
-public class AndroidLauncher extends AndroidApplication {
+public class AndroidLauncher extends AndroidApplication
+{
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new GameMainClass(), config);
 	}
+
 }
