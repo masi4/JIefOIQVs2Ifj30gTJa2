58fe633ce990208bdc563271ab6c0b71576b8fbe
masi4 <meowsqwe@gmail.com>
2017-10-11 19:36:53 +0400

    Создан проект

---
+++ android/src/com/masi4/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.masi4.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.masi4.game.GameMainClass;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameMainClass(), config);
+	}
+}
---
+++ desktop/src/com/masi4/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.masi4.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.masi4.game.GameMainClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameMainClass(), config);
+	}
+}
