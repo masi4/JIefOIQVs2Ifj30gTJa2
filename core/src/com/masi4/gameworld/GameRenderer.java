04df13deb6f0a54fe29a2b1eb5d801e647baa035
U1wknUzeU6 <U1wknUzeU6@gmail.com>
2017-10-26 20:01:23 +0400

    123

--- core/src/com/masi4/gameworld/GameRenderer.java
+++ core/src/com/masi4/gameworld/GameRenderer.java
@@ -6,6 +6,8 @@ package com.masi4.gameworld;
  * Данный класс отвечает за отрисовку игровых объектов
  */
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.masi4.myGame.GameMainClass;
 import com.sun.org.apache.xpath.internal.operations.Or;
@@ -24,7 +26,8 @@ public class GameRenderer {
 
     public void render()
     {
-
+        Gdx.gl.glClearColor(0, 255, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     }
 
 }
